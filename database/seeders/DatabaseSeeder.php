b954170c08106cd704248bd2f45869f489791f23
putrianggiyarani <ptryyyanggiyarani@gmail.com>
2023-07-28 09:08:00 +0700

    admin users dan kategori

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Role;
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        //seeder untuk role
         Role::create([
             'role_name' => 'admin',
         ]);
@@ -22,6 +24,7 @@ class DatabaseSeeder extends Seeder
             'role_name' => 'user',
         ]);
 
+        //seeder untuk user
         User::create([
             'username' => 'admin',
             'email' => 'admin@admin.com',
@@ -34,6 +37,43 @@ class DatabaseSeeder extends Seeder
             'email' => 'user1@user.com',
             'password' => bcrypt('user'),
             'role_id' => 2,
+        ]);
+
+        //seeder untuk kategori
+        Category::create([
+            'name' => 'Flowers Box',
+            'image' => 'box.jpg',
+            'banner' => '1.png',
+        ]);
+
+        Category::create([
+            'name' => 'Fresh Flowers',
+            'image' => 'fresh.jpg',
+            'banner' => '3.png',
+        ]);
+
+        Category::create([
+            'name' => 'Artificial Flowers',
+            'image' => 'arti.jpeg',
+            'banner' => '4.png',
+        ]);
+
+        Category::create([
+            'name' => 'Hand Bouquet',
+            'image' => 'hand.jpg',
+            'banner' => '2.png',
+        ]);
+
+        Category::create([
+            'name' => 'Standing Flowers',
+            'image' => 'standing.jpeg',
+            'banner' => '6.png',
+        ]);
+
+        Category::create([
+            'name' => 'Gift & Hampers',
+            'image' => 'gift.jpg',
+            'banner' => '5.png',
         ]);
     }
 }
---
+++ resources/views/dashboard/layouts/navbar.blade.php
@@ -0,0 +1,26 @@
+<!-- Navbar -->
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      {{-- Make name user in here --}}
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link"><b>admin</b></a>
+      </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('auths.logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                Logout
+            </a>
+            <form id="logout-form" action="{{ route('auths.logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+        </li>
+    </ul>
+  </nav>
+  <!-- /.navbar -->
