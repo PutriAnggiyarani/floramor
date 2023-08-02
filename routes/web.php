c585fb43d9a6ac05e71343455cd79ecf8a71b821
putrianggiyarani <ptryyyanggiyarani@gmail.com>
2023-08-02 22:05:27 +0700

    eror route

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('guest')->group(function () {
     Route::get('/login', [App\Http\Controllers\Auth\AuthController::class, 'index_login'])->name('login');
     Route::post('/login', [App\Http\Controllers\Auth\AuthController::class, 'login'])->name('auths.login');
-
 });
 
 Route::get('/register', [App\Http\Controllers\Auth\AuthController::class, 'index_register'])->name('register');
 Route::post('/register', [App\Http\Controllers\Auth\AuthController::class, 'register'])->name('auths.register');
+
+Route::resource('users', App\Http\Controllers\UserController::class);
+Route::resource('orders', App\Http\Controllers\OrderController::class);
+Route::get('/profile', [App\Http\Controllers\UserController::class, 'show_profile'])->name('profile');
+Route::get('/history', [App\Http\Controllers\OrderController::class, 'show_history'])->name('history');
+
+Route::get('/aboutUs', [App\Http\Controllers\HomeController::class, 'about'])->name('aboutUs');
+Route::get('/ourBrand', [App\Http\Controllers\HomeController::class, 'ourbrand'])->name('ourbrand');
+Route::get('/ourBrand/{id}', [App\Http\Controllers\HomeController::class, 'show_brand'])->name('show_brand');
+
 
 Route::group(['middleware' => ['auth', 'admin']], function () {
     Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboardAdmin');
@@ -28,13 +37,6 @@ Route::group(['middleware' => ['auth', 'admin']], function () {
     Route::resource('products', App\Http\Controllers\ProductController::class);
     Route::post('/logout', [App\Http\Controllers\Auth\AuthController::class, 'logout'])->name('auths.logout');
 });
-
-Route::resource('users', App\Http\Controllers\UserController::class);
-Route::resource('orders', App\Http\Controllers\OrderController::class);
-
-Route::get('/aboutUs', [App\Http\Controllers\HomeController::class, 'about'])->name('aboutUs');
-Route::get('/ourBrand', [App\Http\Controllers\HomeController::class, 'ourbrand'])->name('ourbrand');
-Route::get('/ourBrand/{id}', [App\Http\Controllers\HomeController::class, 'show_brand'])->name('show_brand');
 
 Route::group(['middleware' => ['auth', 'user']], function () {
     Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('homeUser');
